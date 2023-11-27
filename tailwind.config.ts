8d0bae3f0d4cc73a4acddbdc8a6104dbd6836845
Chay23 <scherbiynazar@gmail.com>
2023-11-27 11:53:51 +0200

    Delete unused tailwind custom styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       aspectRatio: {
         '5/7': '5 / 7',
         '3/1': '3 / 1',
