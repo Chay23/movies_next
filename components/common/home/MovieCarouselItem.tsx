e8cbdc1db79c86f20290575651d7d8f7a446a991
Chay23 <scherbiynazar@gmail.com>
2023-12-05 18:07:56 +0200

    Add items needed for the carousel to properly work

---
+++ components/common/home/MovieCarouselItem.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+
+import MovieImage from '../image/MovieImage';
+
+type Props = {
+  movie: movie.Movie;
+};
+
+const MovieCarouselItem = ({ movie }: Props) => {
+  return (
+    <Link
+      href={{
+        pathname: '/movies/[id]',
+        query: { id: movie.id },
+      }}
+      >
+      <article className="relative flex flex-col rounded-xl overflow-hidden">
+        <MovieImage
+          imageSrc={movie.poster_path}
+          serverWidth={400}
+          width={200}
+          height={400}
+          className='hover:opacity-90 transition-all'
+          alt='Movie poster'
+        />
+        <div className='p-2'>
+          <div
+            onMouseEnter={() => {}}
+            className='hover:text-slate-500 transition-all'>
+            {movie.title}
+          </div>
+        </div>
+      </article>
+    </Link>
+  );
+};
+
+export default MovieCarouselItem;
