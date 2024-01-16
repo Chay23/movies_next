66bda685b7159f2458ebeb061be2d7ec36416636
Chay23 <scherbiynazar@gmail.com>
2024-01-16 17:14:53 +0200

    Implement pagination on the Discover page

--- components/common/movies/ExtendedMovieList.tsx
+++ components/common/movies/ExtendedMovieList.tsx
@@ -1,18 +1,39 @@
+import type { ChangeEvent } from 'react';
+
 import { memo } from 'react';
+import { Pagination } from '@mui/material';
 
 import ExtendedMovie from './ExtendedMovie';
 
 type Props = {
   movies: movie.Movie[];
+  page: number;
+  pages: number;
+  handlePageChange: (event: ChangeEvent<unknown>, value: number) => void;
 };
 
-const ExtendedMovieList = ({ movies }: Props) => {
+const ExtendedMovieList = ({
+  movies,
+  page,
+  pages,
+  handlePageChange,
+}: Props) => {
   return (
-    <div className='grid grid-cols-5 gap-10'>
-      {movies.map(movie => (
-        <ExtendedMovie key={movie.id} movie={movie} />
-      ))}
-    </div>
+    <article>
+      <div className='grid grid-cols-5 gap-10'>
+        {movies.map(movie => (
+          <ExtendedMovie key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <div className='flex items-center justify-center pt-20'>
+        <Pagination
+          page={page}
+          // API has restriction for requested page to 500 but response can contain more than 500 pages
+          count={pages >= 500 ? 500 : pages}
+          onChange={handlePageChange}
+        />
+      </div>
+    </article>
   );
 };
 
