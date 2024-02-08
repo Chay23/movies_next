ac3228b0228ad00192f2d36da27d218c0791b8ea
Chay23 <scherbiynazar@gmail.com>
2024-02-08 15:56:26 +0200

    Use Next's ImageProps for MovieImage's component props type

--- components/common/image/MovieImage.tsx
+++ components/common/image/MovieImage.tsx
@@ -1,31 +1,15 @@
-import type { CSSProperties } from 'react';
-
-import Image from 'next/image';
+import Image, { ImageProps } from 'next/image';
 
 type Props = {
   imageSrc: string;
   serverWidth: number | string;
-  width?: number;
-  height?: number;
-  className?: string;
-  fill?: boolean;
-  alt: string;
-  style?: CSSProperties;
-  priority?: boolean;
-  sizes?: string;
-  rest?: any[];
-};
+} & Omit<ImageProps, 'src'>;
 
 const MovieImage = ({
   imageSrc,
   serverWidth,
-  width,
-  height,
   className = '',
   alt,
-  fill = false,
-  style = {},
-  priority = false,
   sizes,
   ...rest
 }: Props) => {
@@ -36,35 +20,25 @@ const MovieImage = ({
     return (
       <Image
         src={`${process.env.NEXT_PUBLIC_API_IMAGE_URL}/${imageWidth}${imageSrc}`}
-        width={width}
-        height={height}
         className={className}
         alt={alt}
-        fill={fill}
-        style={style}
         sizes={
-          sizes
-            ? sizes
-            : '(max-width: 768px) 30vw, (max-width: 1200px) 40vw, 60vw'
+          sizes ?? '(max-width: 768px) 30vw, (max-width: 1200px) 40vw, 60vw'
         }
-        priority={priority}
         {...rest}
       />
     );
   }
+
   return (
     <Image
       src='/static/images/no-picture.png'
-      width={width}
-      height={height}
       className={className + 'border border-gray-200 rounded-xl'}
-      fill={fill}
       style={{ objectFit: 'contain' }}
       alt='No image'
       sizes={
-        fill ? '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' : ''
+        sizes ?? '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
       }
-      priority={priority}
       {...rest}
     />
   );
