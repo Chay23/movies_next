7a2b8b2696f4cfb2d5892fce894b3e5f149ed21c
Chay23 <scherbiynazar@gmail.com>
2024-02-15 16:52:33 +0200

    Fix bug related to searching from the navbar on the Search page

--- hooks/useQueryParams.tsx
+++ hooks/useQueryParams.tsx
@@ -1,11 +1,16 @@
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useQueryParams = (initialQueryParams: Record<string, string>) => {
   const router = useRouter();
   const [queryParams, setQueryParams] = useState(
     new URLSearchParams(initialQueryParams).toString()
   );
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(initialQueryParams);
+    setQueryParams(queryParams.toString());
+  }, [initialQueryParams]);
 
   const updateQueryParams = (queryParamsObj: Record<string, string>) => {
     const queryParams = new URLSearchParams(queryParamsObj);
