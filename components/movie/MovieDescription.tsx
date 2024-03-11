fadd8f36dcadfbe43fa1dcbc3e9aeab620360db8
Chay23 <scherbiynazar@gmail.com>
2024-03-11 12:47:55 +0200

    Move movie title to the movie description component

--- components/movie/MovieDescription.tsx
+++ components/movie/MovieDescription.tsx
@@ -1,5 +1,7 @@
 import type { movie } from '@/typings/movie/movie';
 
+import MovieImage from '../common/image/MovieImage';
+
 import { getMovieDescription } from './utils';
 
 type MovieDescriptionProps = {
@@ -7,17 +9,30 @@ type MovieDescriptionProps = {
 };
 
 const MovieDescription = ({ movie }: MovieDescriptionProps) => {
+  const releaseDate = new Date(movie.release_date);
   const movieDesc = getMovieDescription(movie);
 
   return (
-    <div className='flex flex-col gap-5'>
-      <i>{movie.tagline}</i>
-      {movieDesc.map(item => (
-        <div key={item.title}>
-          <p className='text-lg font-semibold'>{item.title}</p>
-          <p>{item.value}</p>
-        </div>
-      ))}
+    <div className='flex flex-col md:flex-row items-center md:items-start flex-1 gap-y-10 md:gap-x-10'>
+      <div className='relative aspect-2/3 w-full min-w-max max-w-xs'>
+        <MovieImage
+          imageSrc={movie.poster_path}
+          serverWidth={500}
+          fill
+          alt='Movie poster'
+          priority
+        />
+      </div>
+      <div className='flex flex-col gap-5'>
+        <h1>{`${movie.title} (${releaseDate.getFullYear()})`}</h1>
+        <i>{movie.tagline}</i>
+        {movieDesc.map(item => (
+          <div key={item.title}>
+            <p className='text-lg font-semibold'>{item.title}</p>
+            <p>{item.value}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
