c574b65f867ebe7968b15d1ccb0cd74837638e62
Chay23 <scherbiynazar@gmail.com>
2024-03-28 15:37:29 +0200

    Show error for failed genres fetching

--- app/tv/discover/_filters/page.tsx
+++ app/tv/discover/_filters/page.tsx
@@ -1,6 +1,7 @@
 import type { api } from '@/typings/api';
 
 import Filters from './filters';
+import FiltersError from './filtersError';
 
 import { getData } from '@/services/api';
 
@@ -8,7 +9,7 @@ export default async function FiltersPage() {
   const tvGenresRes = await getData<api.GenreResponse>('/genre/tv/list');
 
   if (tvGenresRes.error) {
-    return;
+    return <FiltersError />;
   }
 
   return <Filters genres={tvGenresRes.data.genres} />;
