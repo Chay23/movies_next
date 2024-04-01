86c5637f0a944eb1017bc6396f90527e2583dc9c
Chay23 <scherbiynazar@gmail.com>
2024-04-01 13:59:22 +0300

    Add wrapperClasses prop to FiltersContainer component

--- app/components/tv/discover/filters/FilterContainer.tsx
+++ app/components/tv/discover/filters/FilterContainer.tsx
@@ -1,19 +1,18 @@
-import type { ReactNode } from 'react';
+import type { HTMLAttributes, ReactNode } from 'react';
 
 type Props = {
   title: string;
+  wrapperClasses?: HTMLAttributes<HTMLDivElement>['className'];
   children: ReactNode;
 };
 
-const FilterContainer = ({ title, children }: Props) => {
+const FilterContainer = ({ title, wrapperClasses, children }: Props) => {
   return (
-    <>
+    <article className={wrapperClasses ?? undefined}>
       <hr className='my-4' />
-      <article>
-        <h4 className='text-gray-200 md:text-gray-500 mb-4'>{title}</h4>
-        {children}
-      </article>
-    </>
+      <h4 className='text-gray-200 md:text-gray-500 mb-4'>{title}</h4>
+      {children}
+    </article>
   );
 };
 
