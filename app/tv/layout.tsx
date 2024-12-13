400e2957b08b972516e0930b5afd43dc309cb8b0
Chay23 <scherbiynazar@gmail.com>
2024-12-13 18:47:46 +0200

    Update styling for tv series layout

--- app/tv/layout.tsx
+++ app/tv/layout.tsx
@@ -2,7 +2,7 @@ import type { layout } from '@/typings/layout';
 
 export default function TVShowsLayout({ children }: layout.CommonLayoutProps) {
   return (
-    <main className='relative px-8 md:px-10 lg:px-28 xl:px-40 pt-8 md:pt-12 xl:pt-20 pb-12'>
+    <main className='relative bg-bg-light px-8 md:px-10 lg:px-28 xl:px-64 pt-8 md:pt-12 xl:pt-20 pb-12'>
       {children}
     </main>
   );
