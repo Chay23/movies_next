cccefd1210adc0f1af7fefcae032d7ed8e126a48
Chay23 <scherbiynazar@gmail.com>
2024-12-17 16:58:04 +0200

    Add loading skeleton for series details page

---
+++ app/tv/details/[id]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <>
+      <section className='relative shadow-md p-9 mb-10 rounded-2xl animate-pulse bg-gray-300 min-w-0 w-full h-[450px]'></section>
+      <section className='relative shadow-md p-9 rounded-2xl animate-pulse bg-gray-300 min-w-0 w-full h-[450px]'></section>
+    </>
+  );
+}
